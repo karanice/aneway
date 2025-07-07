9d28faccd3624436e916c7815016732ba10da629
karanice <to.anathema@gmail.com>
2025-07-07 15:01:37 +0300

    Fix project name in service files

--- aneway/urls.py
+++ aneway/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for hexlet_django_blog project.
+URL configuration for aneway project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
--- aneway/wsgi.py
+++ aneway/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hexlet_django_blog project.
+WSGI config for aneway project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hexlet_django_blog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aneway.settings')
 
 application = get_wsgi_application()
